c41a6464815170407bd2958b35b37eb0144ed49b
navabdarveshali <navabdarveshali@gmail.com>
2024-10-02 20:04:34 +0530

    data parsing future added

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))            
+// app.use(cookieParser())
+
 // Database Connection
 connection.connect((err) => {
     if (err) {
